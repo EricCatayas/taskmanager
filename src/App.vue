d4d2b68460bd22d4c27290bbcc98eb987775d5b2
EricCatayas <catayasericjay@gmail.com>
2024-05-19 07:11:30 +0545

    feat: integrate TaskManager and TaskList components in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,32 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <TaskManager/>
+    <TaskForm/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TaskManager from './components/TaskManager.vue'
+import TaskForm from './components/TaskForm.vue';
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TaskManager,
+    TaskForm
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
